1f4adeb267e18e23acfe6cd0804e59354d0fe5eb
Peter <giggsoff@gmail.com>
2017-10-20 17:38:32 +0300

    Update autostart.sh

--- galera/autostart.sh
+++ galera/autostart.sh
@@ -6,5 +6,4 @@ sudo iptables -I INPUT 1 -p tcp --dport 4444 -j ACCEPT
 sudo iptables -I INPUT 1 -p udp --dport 4567 -j ACCEPT
 sed -i 's,^\(safe_to_bootstrap:\).*,\1'1',g' /home/galera/data/grastate.dat
 docker start node0
-sleep 100 && docker exec -ti node0 mysql -p -e 'SET GLOBAL wsrep_provider_options="pc.weight=2";
-
+sleep 100 && docker exec -ti node0 mysql -e 'SET GLOBAL wsrep_provider_options="pc.weight=2";
