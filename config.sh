18af3ee07f104edf21be12ac7d9fe1c25ac08c6f
root <root@localhost.localdomain>
2019-02-26 23:03:22 +0300

    fix proc count

--- config.sh
+++ config.sh
@@ -9,6 +9,7 @@ else
   sed -i 's/CONFIG_CINDER_VOLUMES_SIZE=20G/CONFIG_CINDER_VOLUMES_SIZE=2G/g' answer.cfg
   sed -i 's/CONFIG_NEUTRON_METERING_AGENT_INSTALL=y/CONFIG_NEUTRON_METERING_AGENT_INSTALL=n/g' answer.cfg
   sed -i 's/CONFIG_PROVISION_DEMO=y/CONFIG_PROVISION_DEMO=n/g' answer.cfg
+  sed -i 's/CONFIG_SERVICE_WORKERS=%{::processorcount}/CONFIG_SERVICE_WORKERS=1/g' answer.cfg
   INTERFACE=`ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//"`
   sed -i 's/CONFIG_NEUTRON_OVS_BRIDGE_IFACES=$/CONFIG_NEUTRON_OVS_BRIDGE_IFACES=br-ex:'"$INTERFACE"'/g' answer.cfg
 fi
