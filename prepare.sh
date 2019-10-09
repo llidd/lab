f17ebeca5902f21354c9eefd4e8cf68b6a1fa4dc
root <root@localhost.localdomain>
2019-10-10 00:30:11 +0300

    Now the script is correct

--- prepare.sh
+++ prepare.sh
@@ -3,25 +3,12 @@ if [[ $EUID > 0 ]]; then
   echo "Please run as root/sudo"
   exit 1
 else
-  systemctl stop NetworkManager
-  systemctl disable NetworkManager
-  systemctl stop firewalld
-  systemctl disable firewalld
-  systemctl restart network
-  setenforce 0
-  sed -i 's/SELINUX=enforcing/SELINUX=permissive/g' /etc/selinux/config
-  systemctl start ksmtuned
-  systemctl enable ksmtuned
-  yum install -y centos-release-openstack-rocky
-  yum update -y
-  yum install -y openstack-packstack
-  packstack --gen-answer-file=answer.cfg
-
   #Give more swap space for install if it needed
   mem_number=$(free | grep -v 'used' | awk '{sum+=$2} END {print sum}')
-  minimum_required=$(let 12*1024*1024) # get 12 gb in byte (for me it needed)
-  if [[mem_number < minimum_required]]; then
-    dd if=/dev/zero of=/swapfile bs=1M count=`let (mem_number - minimum_required) / 1024`
+  let "minimum_required= 12*1024*1024" # get 12 gb in byte (for me it needed)
+  if [ $mem_number -lt $minimum_required ]; then
+    let "new_space_mb=(minimum_required-$mem_number)/1024"
+    dd if=/dev/zero of=/swapfile bs=1M count=$new_space_mb
     mkswap /swapfile 
     chmod 600 /swapfile
     swapon /swapfile
